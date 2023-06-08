f93d5977aeaab557e29cf07be72226100003e8e2
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-06-08 21:40:41 +1000

    routes r organised and work, css, button bug

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+
+const apiRoutes = require('./routes/apiRoutes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+app.use('/api', apiRoutes);
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+app.listen(PORT, () => {
+  console.log(`Server listening on PORT ${PORT}`);
+});
--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,29 +1,48 @@
 const express = require('express');
 const fs = require('fs');
 const path = require('path');
-const { v4: uuidv4 } = require('uuid');
 
 const router = express.Router();
 
-// Get all notes
 router.get('/notes', (req, res) => {
-  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json')));
-  res.json(notesData);
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
+    }
+    const notes = JSON.parse(data);
+    res.json(notes);
+  });
 });
 
-// Save a new note
 router.post('/notes', (req, res) => {
-  const newNote = {
-    id: uuidv4(),
-    title: req.body.title,
-    text: req.body.text
-  };
+  const { title, text } = req.body;
 
-  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, '../db.json')));
-  notesData.push(newNote);
-  fs.writeFileSync(path.join(__dirname, '../db.json'), JSON.stringify(notesData));
+  if (!title || !text) {
+    return res.status(400).json({ error: 'Note title and text are required.' });
+  }
 
-  res.json(newNote);
+  fs.readFile('db.json', 'utf8', (err, data) => {
+    if (err) {
+      console.error(err);
+      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
+    }
+    const notes = JSON.parse(data);
+    const newNote = {
+      id: generateId(),
+      title,
+      text,
+    };
+    notes.push(newNote);
+
+    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
+      if (err) {
+        console.error(err);
+        return res.status(500).json({ error: 'An error occurred while saving the note.' });
+      }
+      res.json(newNote);
+    });
+  });
 });
 
 module.exports = router;
