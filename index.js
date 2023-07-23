e23957d3dbaa55aabd714bca5979639adcff6fe8
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-07-23 11:19:40 +1000

    debugging 1: simplifying index.js

--- index.js
+++ index.js
@@ -1,65 +1,29 @@
 const express = require('express');
-const fs = require('fs');
+const apiRoutes = require('./routes/apiRoutes');
 const path = require('path');
 
+
+
 const app = express();
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
-app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
+//Routes
+app.use('/api', apiRoutes);
 
-// API routes
-app.get('/api/notes', (req, res) => {
-  fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
-    }
-    const notes = JSON.parse(data);
-    res.json(notes);
-  });
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
-
-app.post('/api/notes', (req, res) => {
-  const { title, text } = req.body;
-
-  if (!title || !text) {
-    return res.status(400).json({ error: 'Note title and text are required.' });
-  }
-
-  fs.readFile('db.json', 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
-    }
-    const notes = JSON.parse(data);
-    const newNote = {
-      id: generateId(),
-      title,
-      text,
-    };
-    notes.push(newNote);
-
-    fs.writeFile('db.json', JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred while saving the note.' });
-      }
-      res.json(newNote);
-    });
-  });
-});
-
-// HTML routes
 app.get('/notes', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/notes.html'));
+    res.sendFile(path.join(__dirname, './public/notes.html'));
 });
-
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public/index.html'));
+    res.sendFile(path.join(__dirname, './public/index.html'));
 });
 
-app.listen(PORT, () => {
-  console.log(`Server is listening on PORT: ${PORT}`);
-});
+
+app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
+
+
