a3758078c3496bc82df72b5613cd48147411c8ab
Tamara Sferco <122523521+TamaraDawg@users.noreply.github.com>
2023-07-23 11:20:36 +1000

    debugging 2: simplifying/rewriting routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,48 +1,29 @@
-const express = require('express');
-const fs = require('fs');
-const path = require('path');
 
-const router = express.Router();
+const router = require('express').Router();
+const store = require('../db/store');
 
 router.get('/notes', (req, res) => {
-  fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
-    }
-    const notes = JSON.parse(data);
-    res.json(notes);
-  });
+  store
+    .readNotes()
+    .then((notes) => {
+      return res.json(notes);
+    })
+    .catch((err) => res.status(500).json(err));
 });
 
 router.post('/notes', (req, res) => {
-  const { title, text } = req.body;
+  store
+    .makeNote(req.body)
+    .then((note) => res.json(note))
+    .catch((err) => res.status(500).json(err));
+});
 
-  if (!title || !text) {
-    return res.status(400).json({ error: 'Note title and text are required.' });
-  }
 
-  fs.readFile(path.join(__dirname, '../db.json'), 'utf8', (err, data) => {
-    if (err) {
-      console.error(err);
-      return res.status(500).json({ error: 'An error occurred while reading the notes.' });
-    }
-    const notes = JSON.parse(data);
-    const newNote = {
-      id: generateId(),
-      title,
-      text,
-    };
-    notes.push(newNote);
-
-    fs.writeFile(path.join(__dirname, '../db.json'), JSON.stringify(notes), (err) => {
-      if (err) {
-        console.error(err);
-        return res.status(500).json({ error: 'An error occurred while saving the note.' });
-      }
-      res.json(newNote);
-    });
-  });
+router.delete('/notes/:id', (req, res) => {
+  store
+    .deleteNote(req.params.id)
+    .then(() => res.json({ ok: true }))
+    .catch((err) => res.status(500).json(err));
 });
 
 module.exports = router;
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,13 +1,10 @@
-const express = require('express');
 const path = require('path');
+const router = require('express').Router();
 
-const router = express.Router();
-// GET /notes - Serves the notes.html file
 router.get('/notes', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/notes.html'));
 });
 
-// GET * - Serves the index.html file for all other routes
 router.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../public/index.html'));
 });
